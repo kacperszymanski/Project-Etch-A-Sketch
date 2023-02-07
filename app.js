3d0072ef9381e5b120cc0d0593c71fe4d97edf64
Kacper <akacperszymanski@gmail.com>
2023-02-07 22:46:58 +0100

    add color picker

--- app.js
+++ app.js
@@ -1,27 +1,43 @@
 const container = document.getElementById("container");
-const gridButton = document.getElementById("gridButton");
+const gridSizeButton = document.getElementById("gridSizeButton");
+const eraserButton = document.getElementById("eraserButton");
+
+let pickedColor = "black"
 
 function createGrid(size) {
   for (let i = 0; i < size; i++) {
-    
       const row = document.createElement("div");
-      row.classList.add("row");
+
+      row.className = "row";
+      
       for (let j = 0; j < size; j++) {
         const cell = document.createElement("div");
-        cell.classList.add("cell")
-        grid.addEventListener("mouseenter", function() {
-          grid.style.backgroundColor = "black";
-        row.appendChild(cell)
+        cell.className = "cell"
+        cell.addEventListener("mouseenter", function() {
+          cell.style.backgroundColor = pickedColor;
+        
         })
+        row.appendChild(cell)
       }
-      };
-      container.appendChild(row);
+      container.appendChild(row);  
+    };
+      
     }
 
 
 createGrid(16);
 
-gridButton.addEventListener("click", function() {
+eraser.addEventListener("click", function(){
+  pickedColor = "white";
+})
+
+
+eraser.addEventListener("click", function(){
+  pickedColor = "white";
+})
+
+
+gridSizeButton.addEventListener("click", function() {
   container.innerHTML = "";
   const size = prompt("Enter number of squares per side (max 100)");
   if (size > 100) {
@@ -32,3 +48,18 @@ gridButton.addEventListener("click", function() {
 });
 
 
+function createColorPalette(value) {
+  var v = 255/value;
+  for( var rStep = 0, r = 0; rStep < v; rStep++) {    
+      for( var gStep = 0, g = 0; gStep < v; gStep++ ) {       
+          for( var bStep = 0, b = 0; bStep < v; bStep++ ) {                                                  
+              createDiv(r,g,b);
+              b += value;
+          }
+          g += value;
+      }
+      r += value;
+  }
+  createBr();
+}
+createColorPalette()
