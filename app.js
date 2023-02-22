d9ee8060e0b00437b2a25a787167ec5a72014d2e
kacperszymanski <91732728+kacperszymanski@users.noreply.github.com>
2023-02-22 15:39:45 +0100

    add change color function ui

--- app.js
+++ app.js
@@ -1,17 +1,23 @@
 const DEFAULT_COLOR = 'black'
-const DEFAULT_SIZE = 16
+const DEFAULT_SIZE = 17
 const DEFAULT_MODE = 'color'
 
 let currentColor = DEFAULT_COLOR
 let currentMode = DEFAULT_MODE
+let currentSize = DEFAULT_SIZE
 
-function setCurrentMode(newMode){
+function setCurrentMode(newMode) {
   activateButton(newMode)
   currentMode = newMode
 }
 
-function setCurrentColor(newColor){
+function setCurrentColor(newColor) {
+  setCurrentMode('color')
   currentColor = newColor
+}
+
+function setCurrentSize(newGridSize) {
+  currentSize = newGridSize
 }
 
 const grid = document.getElementById("grid");
@@ -19,20 +25,24 @@ const cell = document.getElementById("cell");
 const gridSizeRange = document.getElementById("gridSizeRange");
 const eraserButton = document.getElementById("eraserButton");
 const getColor = document.getElementById("getColor")
-const colorButton = document.getElementById("colorButton")
 const cleanButton = document.getElementById("cleanButton")
 const gridSizeRangeValue = document.getElementById("gridSizeRangeValue")
 
+
 getColor.oninput = (e) => setCurrentColor(e.target.value)
-colorButton.onclick = () => setCurrentMode('color')
+
 eraserButton.onclick = () => setCurrentMode('eraser')
 cleanButton.onclick = () => cleanGrid()
+gridSizeRangeSlider.onmousemove = (e) => updateSizeValue(e.target.value)
+gridSizeRangeSlider.onchange = (e) => changeSize(e.target.value)
+
 
 let mouseDown = false
 document.body.onmousedown = () => (mouseDown = true)
 document.body.onmouseup = () => (mouseDown = false)
 
 function createGrid(size) {
+  grid.innerHTML = ''
   grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`
   grid.style.gridTemplateRows = `repeat(${size}, 1fr)`
 
@@ -45,44 +55,48 @@ function createGrid(size) {
   }
 }
 
-function changeColor(e){
+function changeSize(value) {
+  setCurrentSize(value)
+  updateSizeValue(value)
+  createGrid(value)
+
+}
+
+function changeColor(e) {
   if (e.type === 'mouseover' && !mouseDown) return
   if (currentMode === 'color') {
     e.target.style.backgroundColor = currentColor
-  } else if (currentMode === 'eraser'){
+  } else if (currentMode === 'eraser') {
     e.target.style.backgroundColor = 'white'
   }
 }
 
-function activateButton(e){
-  if (currentMode === 'color'){
+function activateButton(newMode) {
+  if (currentMode === 'color') {
     getColor.classList.remove('activate')
-  } else if (currentMode === 'eraser'){
+  } else if (currentMode === 'eraser') {
     eraserButton.classList.remove('activate')
   }
-if (currentMode === 'color'){
-  getColor.classList.add('activate')
-}else if(currentMode === 'eraser') {
-  eraserButton.classList.add('activate')
-}
+  if (newMode === 'color') {
+    getColor.classList.add('activate')
+  } else if (newMode === 'eraser') {
+    eraserButton.classList.add('activate')
+  }
 }
 
-function cleanGrid(){
+function cleanGrid() {
   const cleanCell = document.getElementsByClassName("cell")
   for (i = 0; i < cleanCell.length; i++)
     cleanCell[i].style.backgroundColor = "white"
 }
 
-function setGridSize(){}
+function updateSizeValue(value) {
+  gridSizeRangeValue.textContent = `${value} x ${value}`
+}
 
-gridSizeRange.addEventListener("input", function () {
-  grid.innerHTML = "";
-  gridSizeRangeValue.textContent = `${gridSizeRange.value} x ${gridSizeRange.value}`
-  createGrid(gridSizeRange.value);
-})
 
 
 window.onload = () => {
-  createGrid(DEFAULT_SIZE)
-  activateButton(DEFAULT_MODE)
+  activateButton(currentMode)
+  changeSize(currentSize)
 }
